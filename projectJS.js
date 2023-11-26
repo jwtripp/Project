8beb446e68fe8015087d4ee2a9b894248ad39018
jwtripp <jwtripp@my.waketech.edu>
2023-11-26 11:53:57 -0500

    Update projectJS.js
    Finished clear form and email validation

--- projectJS.js
+++ projectJS.js
@@ -1,5 +1,20 @@
-document.getElementById("myButton").addEventListener('click',myWindow)
-function myWindow(){
+document.getElementById("myButton").addEventListener('click', validition)// Begins validation
+
+document.getElementById("clearButton").addEventListener('click', function() {
+    myForm.reset();// Clears the form
+    });
+
+function validition() { // Only allows to send with valid email
+    visitorEmail = document.getElementById("myEmail").value;
+
+    if (isValidEmail(visitorEmail)) {
+        myWindow();// Runs myWindow is email is good
+    } else { // Error Message if email is bad
+        alert('Please enter a valid email address.');
+    }
+    }
+
+  function myWindow(){ // Function opens new window to display results
     visitorName = document.getElementById("myName").value;
     visitorEmail = document.getElementById("myEmail").value;
     visitorGoal = document.getElementById("myGoal").value;
@@ -10,7 +25,7 @@ function myWindow(){
     visitorSnack2 = document.getElementById("mySnack2").value;
     visitorDinner = document.getElementById("myDinner").value;
 
-    flyWindow = window.open('Perfectly Portioned', '_blank');
+    flyWindow = window.open('Perfectly Portioned', '_blank'); // Opens window
     flyWindow.document.write('<style>');
     flyWindow.document.write('</style></head><body>');
     flyWindow.document.write('<h1>Your Meal Plan</h1>');
@@ -26,4 +41,9 @@ function myWindow(){
     flyWindow.document.write('<p><strong>Lunch:</strong> ' + myLunch.value.trim() + '</p>');
     flyWindow.document.write('<p><strong>Snack:</strong> ' + mySnack2.value.trim() + '</p>');
     flyWindow.document.write('<p><strong>Dinner:</strong> ' + myDinner.value.trim() + '</p>');
-}
+  }
+
+  function isValidEmail(visitorEmail) {// Validate the email format
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(visitorEmail);
+  }
